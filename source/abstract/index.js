e5d6a0d29af91cc0b2c3a0217102a8757238311a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-06-06 11:06:35 -0700

    Skip keys not present in schema.

--- source/abstract/index.js
+++ source/abstract/index.js
@@ -1,17 +1,28 @@
+import {keys} from "ramda"
 import {mapObjIndexed} from "ramda"
+import {pick} from "ramda"
+import {pipe} from "ramda"
+import {prop} from "ramda"
 
-const coerce = (schema) => (value, key) => schema[key](value)
+const coerce = (schema) => (value, key) => prop(key, schema)(value)
+const coerced = (schema) => pipe(
+  pick(keys(schema)),
+  mapObjIndexed(coerce(schema))
+)
 
 // {...} -> f()
 export default function abstract (configuration) {
   const {name} = configuration
   const {schema} = configuration
+  const attributesFrom = coerced(schema)
 
   return (raw) => {
+    const attributes = attributesFrom(raw)
+
     return {
       type: name,
       raw,
-      attributes: mapObjIndexed(coerce(schema), raw)
+      attributes
     }
   }
 }
