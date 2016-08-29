a6fbfaa1ebb2cdb238caede07ce534b1499784f3
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-29 00:46:09 -0700

    Writing in some error detection

--- source/abstract/index.js
+++ source/abstract/index.js
@@ -7,6 +7,7 @@ import {merge} from "ramda"
 import {map} from "ramda"
 import {identity} from "ramda"
 import {reject} from "ramda"
+import {isNil} from "ramda"
 
 const applicate = (...argument) => (ƒunction) => ƒunction(...argument)
 const coerce = (schema) => (value, key) => prop(key, schema)(value)
@@ -20,11 +21,25 @@ export default function abstract (configuration) {
   const {schema = {}} = configuration
   const {virtuals = {}} = configuration
   const {validations = []} = configuration
+
+  if (isNil(name)) {
+    throw new Error("No name defined for this abstraction")
+  }
+
   const coercedFrom = coerced(schema)
   const virtualizedFrom = virtualized(virtuals)
 
   return (raw) => {
+    if (isNil(raw)) {
+      throw new Error(`No raw data provided to the abstraction ${name}`)
+    }
+
     const data = source(raw)
+
+    if (isNil(data)) {
+      throw new Error(`No data was derived from the source for the abstraction ${name}`)
+    }
+
     const coercedAttributes = coercedFrom(data)
     const virtualizedAttributes = virtualizedFrom(data)
     const attributes = merge(coercedAttributes, virtualizedAttributes)
--- source/abstract/test.js
+++ source/abstract/test.js
@@ -1,4 +1,4 @@
-import {describe, it} from "mocha"
+import {describe, describe as context, it} from "mocha"
 import {expect} from "chai"
 import {last} from "ramda"
 import {split} from "ramda"
@@ -15,60 +15,168 @@ import {number} from "../schema"
 import abstract from "./index"
 
 describe("abstract()", () => {
-  const MINIMUM_AGE = 21
-  const definition = {
-    name: "accounts",
-    source: path(["data", "attributes"]),
-    schema: {
-      name: ignoreNull(text),
-      email: text,
-      age: number,
-      currentCount: number,
-      previousCount: defaultIn(0, number)
-    },
-    virtuals: {
-      emailDomain ({email}) {
-        return last(split("@", email))
-      }
-    },
-    validations: {
-      emailMatchesPattern: pipe(prop("email"), contains("@")),
-      oldEnough: pipe(prop("age"), lte(MINIMUM_AGE))
-    }
-  }
-  const raw = {
-    data: {
-      attributes: {
-        name: null,
-        email: "me@kurtisrainboltgreene.name",
-        age: 19,
-        currentCount: "4",
-        previousCount: null
+  context("with all provided values", () => {
+    const MINIMUM_AGE = 21
+    const definition = {
+      name: "accounts",
+      source: path(["data", "attributes"]),
+      schema: {
+        name: ignoreNull(text),
+        email: text,
+        age: number,
+        currentCount: number,
+        previousCount: defaultIn(0, number)
+      },
+      virtuals: {
+        emailDomain ({email}) {
+          return last(split("@", email))
+        }
+      },
+      validations: {
+        emailMatchesPattern: pipe(prop("email"), contains("@")),
+        oldEnough: pipe(prop("age"), lte(MINIMUM_AGE))
       }
     }
-  }
+    const raw = {
+      data: {
+        attributes: {
+          name: null,
+          email: "me@kurtisrainboltgreene.name",
+          age: 19,
+          currentCount: "4",
+          previousCount: null
+        }
+      }
+    }
 
-  it("returns a record with raw attributes", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("raw.data.attributes.email", "me@kurtisrainboltgreene.name")
+    it("returns a record with raw attributes", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("raw.data.attributes.email", "me@kurtisrainboltgreene.name")
+    })
+
+    it("returns a record with coerced attributes", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("attributes.currentCount", 4)
+    })
+
+    it("returns a record with defaulted attributes", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("attributes.previousCount", 0)
+    })
+
+    it("returns a record with null attributes", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("attributes.name", null)
+    })
+
+    it("returns a record with virtual attributes", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("attributes.emailDomain", "kurtisrainboltgreene.name")
+    })
+
+    it("returns a record with an error field", () => {
+      expect(abstract(definition)(raw)).to.have.deep.property("errors.0", "oldEnough")
+    })
   })
 
-  it("returns a record with coerced attributes", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("attributes.currentCount", 4)
+  context("with no name", () => {
+    const MINIMUM_AGE = 21
+    const definition = {
+      source: path(["data", "attributes"]),
+      schema: {
+        name: ignoreNull(text),
+        email: text,
+        age: number,
+        currentCount: number,
+        previousCount: defaultIn(0, number)
+      },
+      virtuals: {
+        emailDomain ({email}) {
+          return last(split("@", email))
+        }
+      },
+      validations: {
+        emailMatchesPattern: pipe(prop("email"), contains("@")),
+        oldEnough: pipe(prop("age"), lte(MINIMUM_AGE))
+      }
+    }
+    const raw = {
+      data: {
+        attributes: {
+          name: null,
+          email: "me@kurtisrainboltgreene.name",
+          age: 19,
+          currentCount: "4",
+          previousCount: null
+        }
+      }
+    }
+
+    it("throws an error", () => {
+      expect(() => abstract(definition)(raw)).to.throw(Error, "No name defined for this abstraction")
+    })
   })
 
-  it("returns a record with defaulted attributes", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("attributes.previousCount", 0)
+  context("when the raw isn't a value", () => {
+    const MINIMUM_AGE = 21
+    const definition = {
+      name: "accounts",
+      source: path(["data", "attributes"]),
+      schema: {
+        name: ignoreNull(text),
+        email: text,
+        age: number,
+        currentCount: number,
+        previousCount: defaultIn(0, number)
+      },
+      virtuals: {
+        emailDomain ({email}) {
+          return last(split("@", email))
+        }
+      },
+      validations: {
+        emailMatchesPattern: pipe(prop("email"), contains("@")),
+        oldEnough: pipe(prop("age"), lte(MINIMUM_AGE))
+      }
+    }
+    const raw = null
+
+    it("throws an error", () => {
+      expect(() => abstract(definition)(raw)).to.throw(Error, "No raw data provided to the abstraction accounts")
+    })
   })
 
-  it("returns a record with null attributes", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("attributes.name", null)
-  })
+  context("when the source result isn't a value", () => {
+    const MINIMUM_AGE = 21
+    const definition = {
+      name: "accounts",
+      source: () => null,
+      schema: {
+        name: ignoreNull(text),
+        email: text,
+        age: number,
+        currentCount: number,
+        previousCount: defaultIn(0, number)
+      },
+      virtuals: {
+        emailDomain ({email}) {
+          return last(split("@", email))
+        }
+      },
+      validations: {
+        emailMatchesPattern: pipe(prop("email"), contains("@")),
+        oldEnough: pipe(prop("age"), lte(MINIMUM_AGE))
+      }
+    }
+    const raw = {
+      data: {
+        attributes: {
+          name: null,
+          email: "me@kurtisrainboltgreene.name",
+          age: 19,
+          currentCount: "4",
+          previousCount: null
+        }
+      }
+    }
 
-  it("returns a record with virtual attributes", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("attributes.emailDomain", "kurtisrainboltgreene.name")
-  })
-
-  it("returns a record with an error field", () => {
-    expect(abstract(definition)(raw)).to.have.deep.property("errors.0", "oldEnough")
+    it("throws an error", () => {
+      expect(() => abstract(definition)(raw)).to.throw(Error, "No data was derived from the source for the abstraction accounts")
+    })
   })
 })
