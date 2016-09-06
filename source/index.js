851a439e306e70f0faf654d29272d43893a65285
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-06 00:45:25 -0700

    These don't make sense if you don't have these values

--- source/index.js
+++ source/index.js
@@ -1,6 +1,4 @@
 export {default as abstract} from "./abstract"
-export {default as isNewRecord} from "./isNewRecord"
-export {default as isPersisted} from "./isPersisted"
 export {default as isValid} from "./isValid"
 export {boolean} from "./schema"
 export {date} from "./schema"
--- source/isPersisted/index.js
+++
@@ -1,7 +0,0 @@
-import {isNil} from "ramda"
-import {path} from "ramda"
-
-// {attributes: {id, ...}, ...} -> true | false
-export default function isPersisted (record) {
-  return !isNil(path(["attributes", "id"], record))
-}
